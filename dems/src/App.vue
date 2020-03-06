1b264a2257d6caa527a2b7efc9908ad8fef43c02
Brian Smith <briaguya@gmail.com>
2020-03-06 00:27:48 -0600

    make it in vue

---
+++ dems/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Ballot
+      v-bind:options="[
+        'Joe Biden',
+        'Bernie Sanders',
+        'Tulsi Gabbard',
+        'Elizabeth Warren',
+        'Michael Bloomberg',
+        'Amy Klobuchar',
+        'Pete Buttigieg',
+        'Tom Steyer',
+        'Deval Patrick',
+        'Michael Bennet'
+      ]"
+    />
+  </div>
+</template>
+
+<script>
+import Ballot from './components/Ballot.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Ballot
+  }
+};
+</script>
---
+++ dems/src/components/Ballot.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="ballot">
+    <div v-for="option in options" class="option" v-bind:key="option">
+      <input
+        type="radio"
+        v-bind:id="option"
+        v-bind:value="option"
+        v-model="selected"
+      />
+      <label v-bind:for="option">{{ option }}</label>
+    </div>
+
+    <span>Selected: {{ selected }}</span>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Ballot',
+  props: {
+    options: Array
+  },
+  data: function() {
+    return {
+      selected: null
+    };
+  }
+};
+</script>
